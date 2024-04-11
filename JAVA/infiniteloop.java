39f9edaa13369c6e8dce63c2a627580a03d7f97a
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-11 12:22:43 +0530

    Update infiniteloop.java

--- JAVA/infiniteloop.java
+++ JAVA/infiniteloop.java
@@ -1,13 +1,12 @@
 class infiniteloop
 {
-public static void main(String args[])
-{
-int i=0;
-while(i>=0)
-{
-System.out.println(i);
-i=i+1;
-System.out.println(i);
-}
-}
+    public static void main(String args[])
+    {
+        int i=0;
+        while(i>=0)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
 }
