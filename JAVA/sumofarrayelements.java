93815163d955752bc4e9d24ffba8ce10fcd94a00
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-14 18:03:10 +0530

    Update sumofarrayelements.java

--- JAVA/sumofarrayelements.java
+++ JAVA/sumofarrayelements.java
@@ -1,22 +1,21 @@
 import java.util.*;
-public class sumofarrayelements{
-  public static void main(String args[])
-  {
-    Scanner sc=new Scanner(System.in);
-    System.out.print("Enter array size:");
-    int size=sc.nextInt();
-    int array[]=new int[size];
-    int sum=0;
-    int i=0;
-    System.out.print("Enter array elements:");
-    for(i=0;i<array.length;i++)
+class sumofarrayelements{
+    public static void main(String args[])
     {
-      array[i]=sc.nextInt();
+        Scanner sc=new Scanner(System.in); 
+        int sum=0;
+        System.out.println("Enter the array size:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the array elements:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+        }
+        System.out.println("Sum of the array is: "+sum);
     }
-    for(i=0;i<array.length;i++)
-    {
-      sum=sum+array[i];
-    }
-    System.out.print("Sum of array elements is:"+sum);
-  }
 }
