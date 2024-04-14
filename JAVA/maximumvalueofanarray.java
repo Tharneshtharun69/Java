a0858b336230901d3d50792610d89e6c07050f95
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-14 15:41:30 +0530

    Create maximumvalueofanarray.java

---
+++ JAVA/maximumvalueofanarray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class maxofanarray{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        System.out.println("Enter the elenents of the array:");
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The elements present in array are:");
+        for(int i=0;i<size;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println("Maximum value in array is:");
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
