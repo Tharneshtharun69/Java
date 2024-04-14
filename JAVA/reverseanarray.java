e48c4ffff5662bf8e5fc0046bde9f25f37ac98a3
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-14 17:55:03 +0530

    Update reverseanarray.java

--- JAVA/reverseanarray.java
+++ JAVA/reverseanarray.java
@@ -1,8 +1,26 @@
-class reverseanarray {
-    public static void main(String args[]) {
-        int array[] = { 1, 2, 3, 4, 5 };
-        for (int i = array.length - 1; i >= 0; i--) {
-            System.out.print(array[i]);
+import java.util.*;
+public class reverseanarray{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the array size:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the array elements:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Original array is:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Reversed array is:");
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
         }
     }
 }
