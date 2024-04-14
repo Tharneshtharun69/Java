1543aa071281036921772cc5835cbfed77164720
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-14 16:37:03 +0530

    Create minimumvalueofanarray.java

---
+++ JAVA/minimumvalueofanarray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class minimumvalueofanarray{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The elements present in the array are:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Minimum value of the array is:");
+        int min=arr[0]; 
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
