8ab50e15e9abfe0ed5cc2f44f1d6e33a80ea254f
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-19 10:19:19 +0530

    Update factorial.java

--- JAVA/factorial.java
+++ JAVA/factorial.java
@@ -1,6 +1,5 @@
 import java.util.*;
-
-public class factorial {
+class factorial {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
