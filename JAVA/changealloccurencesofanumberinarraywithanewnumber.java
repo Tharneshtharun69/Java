91c11be6dec11ac6e647fa240b7900209480474e
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-20 12:13:26 +0530

    Update changealloccurencesofanumberinarraywithanewnumber.java

--- JAVA/changealloccurencesofanumberinarraywithanewnumber.java
+++ JAVA/changealloccurencesofanumberinarraywithanewnumber.java
@@ -1,32 +1,32 @@
 import java.util.*;
-class changealloccurencesofanumberinarraywithanewnumber
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
+class ChangeAllOccurrencesOfANumberInArrayWithANewNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int[] arr = new int[size];
         System.out.println("Enter the elements of the array:");
-        for(int i=0;i<size;i++)
-        {
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
         System.out.println("Enter the number which needs to be changed:");
-        int needtochange=sc.nextInt();
+        int needToChange = sc.nextInt();
         System.out.println("Enter the new number:");
-        int newnumber=sc.nextInt();
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==needtochange)
-            {
-                arr[i]=newnumber;
+        int newNumber = sc.nextInt();
+        boolean found = false;  
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == needToChange) {
+                arr[i] = newNumber;
+                found = true;  
             }
         }
-        System.out.println("After changing the elements,the array is:");
-        for(int i=0;i<size;i++)
-        {
-            System.out.print(arr[i]+" ");
+        if (!found) {
+            System.out.println("Element not found in array"); 
+        } else {
+            System.out.println("After changing the elements, the array is:");
+            for (int i = 0; i < size; i++) {
+                System.out.print(arr[i] + " ");
+            }
         }
     }
 }
