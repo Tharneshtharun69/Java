7d1471e77771dda058844c343e1eee2cd82f207f
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-20 09:46:41 +0530

    Create addntoallnumbersinanarray.java

---
+++ JAVA/addntoallnumbersinanarray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class addntoallnumbersinanarray{ 
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the number which needs to be added to all the elements of the array:");
+        int n=sc.nextInt();
+        System.out.println("After adding "+n+" to all elements present in the array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+n+" ");
+        }
+    }
+}
