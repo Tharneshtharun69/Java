634462369c050b813f61b537830f1a0ce1d9ffac
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-21 13:11:23 +0530

    Update pairsofarraywhichequalstargetsum.java

--- JAVA/pairsofarraywhichequalstargetsum.java
+++ JAVA/pairsofarraywhichequalstargetsum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class pairsofarraywhichhavetargetsum {
+class pairsofarraywhichequalstargetsum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
