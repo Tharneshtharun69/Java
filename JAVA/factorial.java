8e3a66224e97baa033981ddd7868710f8c98c598
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-25 12:16:26 +0530

    Update factorial.java

--- JAVA/factorial.java
+++ JAVA/factorial.java
@@ -5,7 +5,7 @@ class factorial {
         System.out.println("Enter the value of n:");
         int n = sc.nextInt();
         int fact = 1;
-        for (int i = n; i >= 1; i--) {
+        for (int i = 1; i <= n; i++) {
             fact = fact * i;
         }
         System.out.println("The factorial of "+n+" is:"+fact);
