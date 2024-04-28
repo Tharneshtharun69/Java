fdb9dafd54b11fc80472d3a20ccd05fae8d19e60
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-28 15:52:13 +0530

    Update and rename addnextelementtopresentelementandifitislastelementaddzerotolastelement.java to addnextelementtopresentelementandifitislastelementaddfirstelementtolastelement.java

---
+++ JAVA/addnextelementtopresentelementandifitislastelementaddfirstelementtolastelement.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class AddNextElementToPresentElementAndIfItIsLastElementAddFirstElementToLastElement {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the " + size + " elements of the array:");
+        for (int i = 0; i < size; i++) 
+        {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) 
+        {
+            if (i < size - 1) 
+            {
+                arr[i] = arr[i] + arr[i + 1];
+            } else 
+            {
+                arr[i] = arr[i] + arr[0];
+            }
+        }
+        System.out.println("After adding next elements to the current element and if it is last element, adding first element of the array to the last element, the array is:");
+        for (int i = 0; i < size; i++) 
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
