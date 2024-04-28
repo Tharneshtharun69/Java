2a5f4961c64cc3cab4ca55ae51f23c8c76fadf91
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-04-28 15:53:05 +0530

    Update addnextelementtopresentelementandifitislastelementaddfirstelementtolastelement.java

--- JAVA/addnextelementtopresentelementandifitislastelementaddfirstelementtolastelement.java
+++ JAVA/addnextelementtopresentelementandifitislastelementaddfirstelementtolastelement.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class AddNextElementToPresentElementAndIfItIsLastElementAddFirstElementToLastElement {
+class addnextelementtopresentelementandifitislastelementaddfirstelementtolastelement {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
