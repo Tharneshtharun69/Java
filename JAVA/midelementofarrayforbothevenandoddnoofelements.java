5e66698a27500863f4e4430b89d138535cc5bd2c
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-04 19:26:23 +0530

    Create midelementofarrayforbothevenandoddnoofelements.java

---
+++ JAVA/midelementofarrayforbothevenandoddnoofelements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class midelementofarrayforbothevenandoddnoofelements
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the "+size+" elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        if(size%2==0)
+        {
+            System.out.println("The two mid elements of the array are:");
+            System.out.print(arr[((size/2)-1)]+" "+arr[((size/2))]);
+        }
+        else
+        {
+            System.out.println("The mid element of the array is:");
+            System.out.println(arr[(size%2)+1]);
+        }
+    }
+}
