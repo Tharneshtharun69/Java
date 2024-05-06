b426eb2531c513154c44b426a876baf24a963242
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-06 08:09:04 +0530

    Update sumofnumfrom1tonusingforloop.java

--- JAVA/sumofnumfrom1tonusingforloop.java
+++ JAVA/sumofnumfrom1tonusingforloop.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class sumofnumfrom1ton {
+class sumofnumfrom1tonusingforloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
