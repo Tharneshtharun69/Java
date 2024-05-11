a044f66c80bd756c3aaabc6abb79680cb60107ca
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-11 16:41:56 +0530

    Create Stringconcatenationusingplusoperator.java

---
+++ JAVA/Stringconcatenationusingplusoperator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Stringconcatenationusingplusoperator
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String a=sc.nextLine();
+        System.out.println("Enter the second string:");
+        String b=sc.nextLine();
+        System.out.println("The concatenated string is:");
+        System.out.println(a+b);
+    }
+}
