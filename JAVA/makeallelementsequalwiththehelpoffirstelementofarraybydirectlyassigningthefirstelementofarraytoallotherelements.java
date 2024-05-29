e3884f8b075d9c733b7e667a6dc76ade1cf595d6
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-29 07:19:38 +0530

    Update and rename makeallelementsequalwiththehelpoffirstelementofarray.java to makeallelementsequalwiththehelpoffirstelementofarraybydirectlyassigningthefirstelementofarraytoallotherelements.java

--- JAVA/makeallelementsequalwiththehelpoffirstelementofarraybydirectlyassigningthefirstelementofarraytoallotherelements.java
+++ JAVA/makeallelementsequalwiththehelpoffirstelementofarraybydirectlyassigningthefirstelementofarraytoallotherelements.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class makeallelementsequalwiththehelpoffirstelementofarray
+class makeallelementsequalwiththehelpoffirstelementofarraybydirectlyassigningthefirstelementofarraytoallotherelements
 {
     public static void main(String args[])
     {
