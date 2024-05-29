e2b5d269f9eabd4649a2ae94ab977b005c956dc8
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-29 11:44:49 +0530

    Update and rename factorial.java to factorialusingforloop.java

--- JAVA/factorialusingforloop.java
+++ JAVA/factorialusingforloop.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class factorial {
+class factorialusingforloop {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n:");
