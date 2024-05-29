3a2a23cfb64307b80abd2341e6d508be095dd9f1
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-29 07:06:28 +0530

    Create makeallelementsequalwiththehelpoffirstelementofarray.java

---
+++ JAVA/makeallelementsequalwiththehelpoffirstelementofarray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class makeallelementsequalwiththehelpoffirstelementofarray
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the "+size+" elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=arr[0];
+        }
+        System.out.println("After making all elements equal with the help of the first element of the array,the array is:");
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
