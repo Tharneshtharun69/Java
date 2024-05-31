e58a159354053b01a5ef3d9d404aa87c68dfac89
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-05-31 13:59:21 +0530

    Update multiplicationtablerev.java

--- JAVA/multiplicationtablerev.java
+++ JAVA/multiplicationtablerev.java
@@ -1,16 +1,16 @@
 import java.util.*;
-class multiplicationtablerev
+class multiplicationtableinrev
 {
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the number which you want to print multiplication table:");
-int n=sc.nextInt();
-int result;
-{
-for(int i=20;i>=1;i--){
-System.out.println(i+"x"+n+"="+n*i);
-}
-}
-}
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number for which you want to print multiplication table in reverse from 20th step to 1st step:");
+        int n=sc.nextInt();
+        System.out.print("The multiplication table for "+n+" from 20th step to 1st step is:");
+        System.out.println();
+        for(int i=20;i>=1;i--)
+        {
+            System.out.println(i+"x"+n+"="+n*i);
+        }
+    }
 }
