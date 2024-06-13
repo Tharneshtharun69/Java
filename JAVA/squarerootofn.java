85397d3b4564604977f6702f50ac0a1dd78d28a4
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-13 12:47:14 +0530

    Create squarerootofn.java

---
+++ JAVA/squarerootofn.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class squarerootofn
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
+        System.out.println();
+        System.out.println("The square root of "+n+" is: "+Math.sqrt(n));
+    }
+}
