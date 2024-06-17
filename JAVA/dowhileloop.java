36894d23bd3e5ae24d932c6054af6299dbd7a804
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-17 09:14:43 +0530

    Update dowhileloop.java

--- JAVA/dowhileloop.java
+++ JAVA/dowhileloop.java
@@ -3,11 +3,14 @@ class dowhileloop
 {
     public static void main(String args[])
     {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
         int i=0;
         do{
             System.out.print(i+" ");
             i++;
         }
-        while(i<6);
+        while(i<n);
     }
 }
