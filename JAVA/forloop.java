559b1f78e6e43e5c69b23d44bca5c0651e9a09c4
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-17 09:15:46 +0530

    Update forloop.java

--- JAVA/forloop.java
+++ JAVA/forloop.java
@@ -1,11 +1,14 @@
+import java.util.*;
 class forloop
 {
-public static void main(String args[])
-{
-int a=10;
-for(int i=0;i>a;i++)
-{
-System.out.println(i);
-}
-}
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
 }
