e92894c4bc60dde422f26bd583a298a093c112e5
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-17 17:26:44 +0530

    Update variable_scope.java

--- JAVA/variable_scope.java
+++ JAVA/variable_scope.java
@@ -1,9 +1,16 @@
 class variable_scope
 {
-public static void main(String args[])
-{int x;
-x = 5;
-{ int y = 6;
-System.out.print(x + " " + y);}
-System.out.println(x + " " + y);
-}}
+    public static void main(String args[])
+    {
+        int x=1;
+        int y=2;
+        {
+            int a=x+5;
+            int b=y+6;
+            System.out.print(a + " " + b);
+            //System.out.println(x + " " + y);//error because this variable is initialized and declared outside the block.so its not accessible here
+        }
+        //System.out.print(a+" "+b);//error because this variable is initialized and declared inside the above block.so its not accessible here
+        System.out.println(x + " " + y);
+    }
+}
