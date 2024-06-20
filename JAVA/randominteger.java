0faba52202529625663d4cc226517f06e0b9f04d
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-20 08:41:54 +0530

    Create randominteger.java

---
+++ JAVA/randominteger.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class randominteger
+{
+    public static void main(String args[])
+    {
+        double a=Math.random()*10+1;
+        int b=(int)(a);
+        System.out.print(b);
+    }
+}
