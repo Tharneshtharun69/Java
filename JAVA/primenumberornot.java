bfe34a3bf76fc587900d9a77411d66d893edc166
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-22 18:18:09 +0530

    Create primenumberornot.java

---
+++ JAVA/primenumberornot.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class primenumberornot {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        int count = 0;
+        if (n % 1 == 0) {
+            count++;
+        }
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.print("Prime number");
+        } else {
+            System.out.print("Not a Prime number");
+        }
+    }
+}
