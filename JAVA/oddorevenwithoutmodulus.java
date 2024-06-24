2337c623c9800cf99f07a357f3f59bf3f8739c3a
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-24 12:09:35 +0530

    Update oddorevenwithoutmodulus.java

--- JAVA/oddorevenwithoutmodulus.java
+++ JAVA/oddorevenwithoutmodulus.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
-class oddorevenwithoutmodulus
+import java.util.*;
+class oddorevenwithoutmodulo
 {
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter a number:");
-int n=sc.nextInt();
-int answer=n/2;
-if(answer*2==n)
-{
-System.out.println("The number is even");
-}
-else
-{
-System.out.println("The number is odd");
-}
-}
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
+        if((n/2)*2==n)
+        {
+            System.out.print("even");
+        }
+        else
+        {
+            System.out.print("odd");
+        }
+    }
 }
