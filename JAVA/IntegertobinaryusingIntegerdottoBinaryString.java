3a29387dbd58f94fbd753651871cb77f8dee2027
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-24 14:12:12 +0530

    Create IntegertobinaryusingIntegerdottoBinaryString.java

---
+++ JAVA/IntegertobinaryusingIntegerdottoBinaryString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class IntegertobinaryusingIntegerdottoBinaryString
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
+        System.out.print(Integer.toBinaryString(n));
+    }
+}
