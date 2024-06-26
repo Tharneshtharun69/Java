434330aaf55e6e7af3caeede65c5df08f808c896
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-26 11:20:56 +0530

    Create convertintegerstringtointeger.java

---
+++ JAVA/convertintegerstringtointeger.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class convertintegerstringtointeger
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a no:");
+        String s=sc.nextLine();
+        int a=Integer.parseInt(s);
+        System.out.print(a);
+    }
+}
