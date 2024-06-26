8a390a2b0263f009ca00fcd634faea0d76657ed2
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-26 10:00:00 +0530

    Update and rename Stringreverse.java to Stringreverseusingforloop.java

--- JAVA/Stringreverseusingforloop.java
+++ JAVA/Stringreverseusingforloop.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Stringreverse {
+class Stringreverseusingforloop {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the string: ");
