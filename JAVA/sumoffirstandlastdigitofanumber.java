d3147ec38140d0eec37747f5272c4bb861d1da9f
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-26 17:27:48 +0530

    Create sumoffirstandlastdigitofanumber.java

---
+++ JAVA/sumoffirstandlastdigitofanumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+   public static void  main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int sum=0;
+      for(int i=1;n>0;i++){
+          if(i==1){
+              sum=sum+n%10;
+          }
+          if((n/10)==0){
+              sum=sum+n%10;
+          }
+          n=n/10;
+      }
+      System.out.println(sum);
+    }
+}
