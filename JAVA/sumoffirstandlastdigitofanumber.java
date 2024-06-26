bf97aec19e377512e768342663980e8fe636c41e
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-26 17:28:29 +0530

    Update sumoffirstandlastdigitofanumber.java

--- JAVA/sumoffirstandlastdigitofanumber.java
+++ JAVA/sumoffirstandlastdigitofanumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+class sumoffirstandlastdigitofanumber{
    public static void  main(String args[]){
       Scanner sc = new Scanner(System.in);
       int n = sc.nextInt();
