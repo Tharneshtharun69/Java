e8ddb8fa388e57df8640841fba0bb8561b8b2dcc
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-06-26 13:58:23 +0530

    Update and rename binarytodecimal.java to binarytodecimalusingIntegerclass.java

--- JAVA/binarytodecimalusingIntegerclass.java
+++ JAVA/binarytodecimalusingIntegerclass.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class binarytodecimal{
+class binarytodecimalusingIntegerclass{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a binary value:");
