70eb295c095043da61cc3df1a74dcff7520f2c06
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-01 17:49:35 +0530

    Update and rename arraysorting.java to arraysortingwithoutusingarraysdotsort.java

--- JAVA/arraysortingwithoutusingarraysdotsort.java
+++ JAVA/arraysortingwithoutusingarraysdotsort.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-class arraysorting {
+class arraysortingwithoutusingarraysdotsort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int size=sc.nextInt();
