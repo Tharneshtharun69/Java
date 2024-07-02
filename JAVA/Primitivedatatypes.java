4faa07a8e942412a78da8c393760f08899fe1361
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-02 12:10:33 +0530

    Create Primitivedatatypes.java

---
+++ JAVA/Primitivedatatypes.java
@@ -0,0 +1,18 @@
+class Primitivedatatypes {
+    public static void main(String[] args) {
+        int a=130;
+        float b=678.123f;
+        double c=345.654654665467;
+        long d=12324545654677878L;
+        short e=130;
+        boolean f=true;
+        byte g=120;
+        System.out.println("integer:"+a);
+        System.out.println("float:"+b);
+        System.out.println("double:"+c);
+        System.out.println("long:"+d);
+        System.out.println("short:"+e);
+        System.out.println("boolean:"+f);
+        System.out.println("byte:"+g);
+    }
+}
