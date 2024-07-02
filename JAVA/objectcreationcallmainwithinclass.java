b4d7a2fe2cd3031c6ba8d518975a6778dfdac113
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-02 09:47:28 +0530

    Create objectcreationcallmainwithinclass.java

---
+++ JAVA/objectcreationcallmainwithinclass.java
@@ -0,0 +1,11 @@
+class objectcreationcallmainwithinclass
+{
+  int rollno=56;
+  String name="Tharnesh";
+  public static void main(String args[])
+  {
+    objectcreationcallmainwithinclass o=new objectcreationcallmainwithinclass();
+    System.out.println(o.rollno);
+    System.out.println(o.name);
+  }
+}
