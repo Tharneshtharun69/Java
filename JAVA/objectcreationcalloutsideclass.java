a78ce528e105355cf8606847b8e08aa0f901a1d5
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-02 09:54:25 +0530

    Create objectcreationcalloutsideclass.java

---
+++ JAVA/objectcreationcalloutsideclass.java
@@ -0,0 +1,14 @@
+class outside
+{
+  int rollno=56;
+  String name="Tharnesh";
+}
+class objectcreationcalloutsideclass
+{
+  public static void main(String args[])
+  {
+    outside o=new outside();
+    System.out.print(o.rollno);
+    System.out.print(o.name)l
+  }
+}
