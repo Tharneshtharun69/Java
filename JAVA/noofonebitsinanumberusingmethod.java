7432238dbfa207f2c653e5d864bef355a11669fb
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-02 15:48:50 +0530

    Create noofonebitsinanumberusingmethod.java

---
+++ JAVA/noofonebitsinanumberusingmethod.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class noofonebitsinanumberusingmethod{
+    public int noofonebitsinano(int n) {
+        String binary=Integer.toBinaryString(n);
+        int count=0;
+        for(int i=0;i<binary.length();i++)
+        {
+            if(binary.charAt(i)=='1')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      System.out.print(noofonebitsinano(n));
+    }
+}
