90e3449d123462657b7c1be6dab7fe5794343324
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-02 16:00:28 +0530

    Create noofzerosinabinaryrepresentationofanumberusingmethod.java

---
+++ JAVA/noofzerosinabinaryrepresentationofanumberusingmethod.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class noofzerosinabinaryrepresentationofanumberusingmethod{
+    public static int noofzerosinabinaryrepresentationofanumber(int n) {
+        String binary=Integer.toBinaryString(n);
+        int count=0;
+        for(int i=0;i<binary.length();i++)
+        {
+            if(binary.charAt(i)=='0')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      System.out.print(noofzerosinabinaryrepresentationofanumber(n));
+    }
+}
