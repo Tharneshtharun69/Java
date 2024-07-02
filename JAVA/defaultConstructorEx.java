c4675a4292854e2222a41c36fb79e85e09a14451
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-02 11:38:42 +0530

    Create defaultConstructorEx.java

---
+++ JAVA/defaultConstructorEx.java
@@ -0,0 +1,10 @@
+class defaultConstructorEx{
+  ConstructorEx()//constructor
+  { //constructor
+    System.out.println("Welcome Everyone");
+  }
+  public static void main(String args[])
+  {
+    ConstructorEx c=new ConstructorEx();
+  }
+}
