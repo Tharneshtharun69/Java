f447b5704c779fbdb9a4ac5184a9e5a2e1d48206
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-04 18:44:55 +0530

    Create gcdoftwonumbers.java

---
+++ JAVA/gcdoftwonumbers.java
@@ -0,0 +1,27 @@
+class gcdoftwonumbers
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int b=sc.nextInt();
+    int max=0;
+    if(n>b)
+    {
+      max=n;
+    }
+    else
+    {
+      max=b;
+    }
+    int gcd=0;
+    for(int i=1;i<=max;i++)
+    {
+      if(n%i==0 && b%i==0)
+      {
+        gcd=i;
+      }
+    }
+    System.out.println(gcd);
+  }
+}
