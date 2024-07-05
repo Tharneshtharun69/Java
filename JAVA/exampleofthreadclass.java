6092713da52cbd7fb144388cd4a949940452271c
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-05 10:39:28 +0530

    Create exampleofthreadclass.java

---
+++ JAVA/exampleofthreadclass.java
@@ -0,0 +1,9 @@
+public class exampleofthreadclass implements Runnable{
+  public void run(){
+    System.out.println("Thread started running");
+  }
+  public static void main (String[] args) {
+    Runnable r=new info();
+    Thread t=new Thread()
+  }
+}
