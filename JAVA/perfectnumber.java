dea58cc9c287f4dd5e2b7126e708354735412ba2
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-05 14:18:51 +0530

    Update perfectnumber.java

--- JAVA/perfectnumber.java
+++ JAVA/perfectnumber.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
-class perfectnumber //ifsumofdivisorsequalssamenoitisperfectnumber
+class perfectnumberornot//ifsumofdivisorsequalssamenoitisperfectnumber
 {
    public static void  main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      int N = sc.nextInt();
-      int i;
-      int sum=0;
-      for(i=1;i<=N;i++){
-        if(N%i==0){
-            if(i!=N){
-                sum=sum+i;
-            }
-        }
-      }
-      if(sum==N){
-        System.out.println("Yes");
-      }
-      else{
-        System.out.println("No");
-      }
-    }
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+       int i;
+       int sum=0;
+       for(i=1;i<=N;i++)
+       {
+           if(N%i==0)
+           {
+               sum=sum+i;
+           }
+       }
+       if(sum==N)
+       {
+           System.out.println("Yes");
+       }
+       else
+       {
+           System.out.println("No");
+       }
+   }
 }
