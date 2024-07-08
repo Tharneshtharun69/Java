b0f576bc74dbef9195d20c4e219a207ee1602d33
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-08 08:06:56 +0530

    Update secondmaximumelementinanarray.java

--- JAVA/secondmaximumelementinanarray.java
+++ JAVA/secondmaximumelementinanarray.java
@@ -1,35 +1,28 @@
 import java.util.*;
 class secondmaximumelementinanarray {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        System.out.println("Enter the "+size+" elements of the array:");
-        for(int i=0;i<size;i++)
-        {
-            arr[i]=sc.nextInt();
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the " + size + " elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        int max=0;
-        for(int i=0;i<size;i++)
-        {
-            if(arr[i]>max)
-            {
-                max=arr[i];
+        int max = Integer.MIN_VALUE;
+        int secondmax = Integer.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > max) {
+                secondmax = max;
+                max = arr[i];
+            } else if (arr[i] > secondmax && arr[i] != max) {
+                secondmax = arr[i];
             }
         }
-        int secondmax=0;
-        for(int i=0;i<size;i++)
-        {
-            if(arr[i]==max)
-            {
-                i++;
-            }
-            else
-            {
-                secondmax=arr[i];
-            }
+        if (secondmax == Integer.MIN_VALUE) {
+            System.out.println("No second max element found (all elements might be the same).");
+        } else {
+            System.out.println("Second max element of the array is: " + secondmax);
         }
-        System.out.println("Second max element of the array is: "+secondmax);
     }
 }
