776e97be91fcaefa461ec5a11b145e613b653032
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-08 10:38:58 +0530

    Create Arraylistreverse.java

---
+++ JAVA/Arraylistreverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Arraylistreverse
+{
+  public static void main(String args[])
+  {
+    ArrayList obj=new ArrayList();
+    obj.add("Tokyo");
+    obj.add("Rio");
+    obj.add("Helsinki");
+    obj.add("Oslo");
+    Iterator itr=obj.iterator();
+    for(int i=obj.size()-1;i>=0;i--)
+    {
+      System.out.print(obj.get(i)+" ");
+    }
+  }
+}
