c9262a91a279609f0803d1433be0f5d03b83a6f0
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-09 11:56:40 +0530

    Update and rename removevowelsinastringusingstringbuilder.java to removevowelsinastringandprintthestringusingstringbuilder.java

--- JAVA/removevowelsinastringandprintthestringusingstringbuilder.java
+++ JAVA/removevowelsinastringandprintthestringusingstringbuilder.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class removevowelsinastringusingstringbuilder
+class removevowelsinastringandprintthestringusingstringbuilder
 {
   public static void main (String[] args) 
   {
