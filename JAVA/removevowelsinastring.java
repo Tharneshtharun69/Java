a471494a6ed5751866eb1d0bcb16cb4ef1a5d21f
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-09 11:55:01 +0530

    Create removevowelsinastring.java

---
+++ JAVA/removevowelsinastring.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class removevowelsinastring
+{
+  public static void main (String[] args) 
+  {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    StringBuilder sb=new StringBuilder();
+    for(int i=0;i<s.length();i++)
+    {
+      if(s.charAt(i)!='a'&&s.charAt(i)!='e'&&s.charAt(i)!='i'&&s.charAt(i)!='o'&&s.charAt(i)!='u'&&s.charAt(i)!='A'&&s.charAt(i)!='E'&&s.charAt(i)!='I'&&s.charAt(i)!='O'&&s.charAt(i)!='U')
+      {
+        sb.append(s.charAt(i));
+      }
+    }
+    String a=sb.toString();
+    System.out.print(a);
+  }
+}
