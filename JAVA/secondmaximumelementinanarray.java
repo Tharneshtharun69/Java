f5816968caa810ac884ad40a940f9f89e4e1b8ad
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-20 09:17:33 +0530

    Update secondmaximumelementinanarray.java

--- JAVA/secondmaximumelementinanarray.java
+++ JAVA/secondmaximumelementinanarray.java
@@ -1,28 +1,48 @@
-import java.util.*;
-class secondmaximumelementinanarray {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array:");
-        int size = sc.nextInt();
-        int arr[] = new int[size];
-        System.out.println("Enter the " + size + " elements of the array:");
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
+import java.util.Scanner;
+class secondmaxelementinthearray
+{ 
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.print("Enter the elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
         }
-        int max = Integer.MIN_VALUE;
-        int secondmax = Integer.MIN_VALUE;
-        for (int i = 0; i < size; i++) {
-            if (arr[i] > max) {
-                secondmax = max;
-                max = arr[i];
-            } else if (arr[i] > secondmax && arr[i] != max) {
-                secondmax = arr[i];
+        System.out.print("The array is:");
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        int max=arr[0];
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
             }
         }
-        if (secondmax == Integer.MIN_VALUE) {
-            System.out.println("No second max element found (all elements might be the same).");
-        } else {
-            System.out.println("Second max element of the array is: " + secondmax);
+        int secondmax=0;
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]==max)
+            {
+                continue;
+            }
+            else if(arr[i]>secondmax)
+            {
+                secondmax=arr[i];
+            }
+            else
+            {
+                i++;
+            }
         }
+        System.out.println();
+        System.out.print("The second maximum element of the array is:");
+        System.out.print(secondmax);
     }
 }
