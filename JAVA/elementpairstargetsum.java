7c408b6ede35afaf474545119d5e8d70f11c2f8b
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-21 15:28:50 +0530

    Create elementpairstargetsum.java

---
+++ JAVA/elementpairstargetsum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class elementpairstargetsum{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.print("Enter the elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("The array is:");
+        System.out.println();
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Enter the targetsum:");
+        int targetsum=sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]+arr[j]==targetsum)
+                {
+                    if(!found)
+                    {
+                        System.out.println("Element pairs which sum to obtain target sum: ");
+                        found=true;
+                    }
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+        if(found==false)
+        {
+            System.out.print("No two elements found which sum to target sum");
+        }
+    }
+}
