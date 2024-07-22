5bc75e7e1be4c1a57d2385fdce998a87702fa1fe
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-22 07:31:44 +0530

    Update secondmaximumelementofthearray.java

--- JAVA/secondmaximumelementofthearray.java
+++ JAVA/secondmaximumelementofthearray.java
@@ -23,7 +23,7 @@ class secondmaximumelementofthearray {
                 max=arr[i];
             }
         }
-        int secondmax=0;
+        int secondmax=arr[0];
         for(int i=0;i<size;i++)
         {
             if(arr[i]<max&&arr[i]>secondmax)
