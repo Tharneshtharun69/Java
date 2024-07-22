8b494b80747f616332b7a9cb1638b32bab16a150
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-22 07:28:56 +0530

    Update JAVA

---
+++ JAVA/secondmaximumelementofthearray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class secondmaximumelementofthearray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The array is:");
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        int max=arr[0];
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        int secondmax=0;
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]<max&&arr[i]>secondmax)
+            {
+                secondmax=arr[i];
+            }
+        }
+        System.out.println();
+        System.out.println("Second maximum element of the array is:");
+        System.out.println(secondmax);
+    }
+}
