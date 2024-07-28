ad4843b84a7c14bbaece9c83c387d2f2d4ab5534
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-07-28 08:01:20 +0530

    Create sumofoddsigitsandevendigitsinanumber.java

---
+++ JAVA/sumofoddsigitsandevendigitsinanumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class sumofoddsigitsandevendigitsinanumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
+        int oddsum=0;
+        int evensum=0;
+        int r;
+        while(n!=0)
+        {
+            r=n%10;
+            if(r%2==0)
+            {
+                evensum=evensum+r;
+            }
+            else
+            {
+                oddsum=oddsum+r;
+            }
+            n/=10;
+        }
+        System.out.println("Odd sum: "+oddsum);
+        System.out.println("Even sum: "+evensum);
+    }
+}
