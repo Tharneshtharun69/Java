a2a56fa40edeb7c86a5204962b0a6e4c7f4c8574
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-08-23 07:54:30 +0530

    Update Fibonacciseries.java

--- JAVA/Fibonacciseries.java
+++ JAVA/Fibonacciseries.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 class fibonacciseries{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the no of terms that you want to print Fibonacci series:");
         int n=sc.nextInt();
         int first=0;
         int second=1;
-        System.out.print(first+" "+second+" ");
         int next;
-        for(int i=2;i<n;i++)
+        System.out.print(first+" "+second+" ");
+        for(int i=3;i<=n;i++)
         {
             next=first+second;
             System.out.print(next+" ");
