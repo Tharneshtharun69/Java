0ad614e977b9e87b79d380e4dc671f02d1214e16
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-11-11 10:59:35 +0530

    Create armstrongnumberslessthann.java

---
+++ JAVA/armstrongnumberslessthann.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class armstrongnumberslessthann
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r;
+        for(int i=1;i<=n;i++)
+        {
+            int sum=0;
+            int a=i;
+            int b=a;
+            int noofdigits=Integer.toString(a).length();
+            while(a!=0)
+            {
+                r=a%10;
+                sum=sum+(int)Math.pow(r,noofdigits);
+                a/=10;
+            }
+            if(sum==b)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
