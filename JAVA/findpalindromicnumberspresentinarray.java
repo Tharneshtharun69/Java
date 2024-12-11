69f118196a0654b4a83aaa115cef8c9ad2feaa18
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-12-11 10:49:40 +0530

    Create findpalindromicnumberspresentinarray.java

---
+++ JAVA/findpalindromicnumberspresentinarray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class findpalindromicnumberspresentinarray
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.print("Enter the size of the array:");
+	    int size=sc.nextInt();
+	    int arr[]=new int[size];
+	    boolean found=false;
+	    ArrayList<Integer> list=new ArrayList<>();
+	    System.out.print("Enter the elements of the array:");
+	    for(int i=0;i<size;i++)
+	    {
+	      arr[i]=sc.nextInt();
+	    }
+	    for(int i=0;i<size;i++)
+	    {
+	        int n=arr[i];
+	        int ori=n;
+	        int r;
+	        int rev=0;
+	        while(n!=0)
+	        {
+	            r=n%10;
+	            rev=(rev*10)+r;
+	            n/=10;
+	        }
+	        if(ori==rev)
+	        {   
+	            found=true;
+	            System.out.print(ori+" ");
+	        }
+	    }
+	    if(found==false)
+	    {
+	        System.out.print("There are no palindromic numbers in the array");
+	    }
+	}
+}
