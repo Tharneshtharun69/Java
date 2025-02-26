78027a292cdaae9fdb2b39e0e66946fd47ccdfbe
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2025-02-26 09:24:55 +0530

    Update JAVA

---
+++ JAVA/nextprimeno.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class nextprimeno{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>=n;i++)
+        {
+            int divisorcount=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    divisorcount++;
+                }
+            }
+            if(divisorcount==2)
+            {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
