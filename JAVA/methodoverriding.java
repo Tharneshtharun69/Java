a11bb427f0d5da78b73b1c48679e7208e62bc1d7
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2025-04-26 19:33:43 +0530

    Update methodoverriding.java

--- JAVA/methodoverriding.java
+++ JAVA/methodoverriding.java
@@ -1,24 +1,27 @@
-//same name,different type,different parameters
 class BaseCalculator {
     // Method to add two integers
     public int add(int a, int b) {
         return a + b;
     }
 }
+
 class ExtendedCalculator extends BaseCalculator {
-    // Overrides the 'add' method to work with three integers
-    public int add(int a, int b, int c) {
-        return a + b + c;
+    // Correctly overriding the 'add' method with same parameters
+    @Override
+    public int add(int a, int b) {
+        System.out.println("ExtendedCalculator overriding BaseCalculator method.");
+        return a + b + 10; // Just to show it's overridden (you can customize)
     }
 }
+
 class methodoverriding {
     public static void main(String[] args) {
         // Create an instance of BaseCalculator
         BaseCalculator baseCalc = new BaseCalculator();
         System.out.println("Sum of two integers (Base): " + baseCalc.add(2, 3));
+
         // Create an instance of ExtendedCalculator
         ExtendedCalculator extendedCalc = new ExtendedCalculator();
-        System.out.println("Sum of two integers (Extended): " + extendedCalc.add(2, 3)); // Calls BaseCalculator's method
-        System.out.println("Sum of three integers: " + extendedCalc.add(2, 3, 4)); // Calls overridden method
+        System.out.println("Sum of two integers (Extended): " + extendedCalc.add(2, 3)); // Calls overridden method
     }
 }
